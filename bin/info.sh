8720ab141c3a054c79c11c00d8793fa23f574835
Mario Solano <marioqwe@vt.edu>
2019-01-24 12:41:55 -0500

    use ssl/site-dir parameters instead of hardcoding pwd

--- bin/info.sh
+++ bin/info.sh
@@ -2,10 +2,36 @@
 
 # Get additional information about certificates at the specified location.
 
+POSITIONAL=()
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case ${key} in
+        --ssl-dir)
+            SSL_DIR=$2
+            shift
+            shift
+            ;;
+        --site-dir)
+            shift
+            shift
+            ;;
+        *)
+            POSITIONAL+=("$1")
+            shift
+            ;;
+    esac
+done
+
+if [ -z ${SSL_DIR+x} ];
+then
+    echo "InternalError: missing 'ssl' directory."
+    exit -1
+fi
+
 docker run --rm -it --name certbot \
--v "$(pwd)"/ssl/etc/letsencrypt:/etc/letsencrypt \
--v "$(pwd)"/ssl/var/lib/letsencrypt:/var/lib/letsencrypt \
--v "$(pwd)"/site:/data/letsencrypt \
-certbot/certbot \
---staging \
-certificates "$@"
+-v "{SSL_DIR}"/etc/letsencrypt:/etc/letsencrypt \
+-v "{SSL_DIR}"/var/lib/letsencrypt:/var/lib/letsencrypt \
+certbot/certbot certificates
--- bin/test.sh
+++ bin/test.sh
@@ -7,11 +7,53 @@
 # the actual commands. That way we'll not use up
 # our '20 certificates / 7 days' limit.
 
+POSITIONAL=()
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case ${key} in
+        --ssl-dir)
+            SSL_DIR=$2
+            shift
+            shift
+            ;;
+        --site-dir)
+            SITE_DIR=$2
+            shift
+            shift
+            ;;
+        --email)
+            shift
+            shift
+            ;;
+        *)
+            POSITIONAL+=("$1")
+            shift
+            ;;
+    esac
+done
+
+if [ -z ${SSL_DIR+x} ];
+then
+    echo "InternalError: missing 'ssl' directory."
+    exit -1
+fi
+
+if [ -z ${SITE_DIR+x} ];
+then
+    echo "InternalError: missing 'site' directory."
+    exit -1
+fi
+
+set -- "${POSITIONAL[@]}"
+
 docker run -it --rm \
--v "$(pwd)"/ssl/etc/letsencrypt:/etc/letsencrypt \
--v "$(pwd)"/ssl/var/lib/letsencrypt:/var/lib/letsencrypt \
--v "$(pwd)"/ssl/var/log/letsencrypt:/var/log/letsencrypt \
--v "$(pwd)"/site:/data/letsencrypt \
+-v "${SSL_DIR}"/etc/letsencrypt:/etc/letsencrypt \
+-v "${SSL_DIR}"/var/lib/letsencrypt:/var/lib/letsencrypt \
+-v "${SSL_DIR}"/var/log/letsencrypt:/var/log/letsencrypt \
+-v "${SITE_DIR}":/data/letsencrypt \
 certbot/certbot \
 certonly --webroot \
 --webroot-path=/data/letsencrypt \
